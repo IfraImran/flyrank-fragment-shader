75a4f8e2d37c3032eb62d8053c4a8bc4625a4e8c
IfraImran <ifraimran56@gmail.com>
2026-07-13 18:11:06 +0500

    Create Hero.jsx

---
+++ Hero.jsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="overlay">
+        <h1>Ifra Portfolio</h1>
+        <p>Custom GLSL Fragment Shader Hero</p>
+        <button>Explore</button>
+      </div>
+    </section>
+  );
+}
